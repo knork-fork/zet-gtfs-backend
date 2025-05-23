a59f366d6d528e2b3480c6263c2571c0b8ebcb71
Luka Knezic <luka@giscloud.com>
2025-05-23 05:32:50 +0200

    Display more info on info endpoint

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Response\JsonResponse;
+use App\Service\CachedDataService;
 use KnorkFork\LoadEnvironment\Environment;
 
 final class StatusController
@@ -17,13 +18,22 @@ final class StatusController
 
     public static function info(): JsonResponse
     {
-        $cronPollInterval = Environment::getStringEnv('POLLING_INTERVAL_IN_SECONDS');
+        $pollInterval = Environment::getStringEnv('POLLING_INTERVAL_IN_SECONDS');
+        $inactivityTime = Environment::getStringEnv('STOP_POLLING_AFTER_INACTIVITY_IN_SECONDS');
+        $lastCacheRead = filemtime(CachedDataService::LAST_CACHE_READ_FILENAME);
+        $lastCacheWrite = filemtime(CachedDataService::GTFS_CACHE_FILENAME);
         $zetUrl = Environment::getStringEnv('ZET_URL');
+
+        $isCurrentlyPolling = (time() - $lastCacheRead) < $inactivityTime;
 
         return new JsonResponse([
             'description' => 'GTFS-RT to JSON converter',
-            'polling_interval_in_seconds' => $cronPollInterval,
             'zet_url' => $zetUrl,
+            'polling_interval_in_seconds' => $pollInterval,
+            'stop_polling_after_inactivity_in_seconds' => $inactivityTime,
+            'last_cache_read' => $lastCacheRead,
+            'last_cache_write' => $lastCacheWrite,
+            'is_currently_polling' => $isCurrentlyPolling,
         ]);
     }
 }
