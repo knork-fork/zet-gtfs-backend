e4e8a5e8cad96b31f7a138defe477d9850d83994
Luka Knezic <luka@giscloud.com>
2026-02-13 21:01:00 +0100

    Add Google Search Console verification for zet-uzivo.com

--- scripts/deployment/build_frontend.sh
+++ scripts/deployment/build_frontend.sh
@@ -34,10 +34,12 @@ if ! grep -qi '<meta[^>]*name=["'\'']description["'\'']' "$FILE"; then
     <meta name=\"description\" content=\"${DESCRIPTION}\">" "$FILE"
 fi
 
-# <meta name="google-site-verification" content="mi7cJVnuoE7_yc8nlz_MZU8ZSPR6_OzdWUgxXrqJF1A" />
-# Insert Google Search Console verification
+# Insert Google Search Console verification for zet.knork-studio.com (legacy)
 sed -i "/<meta name=\"description\"/a\\
     <meta name=\"google-site-verification\" content=\"mi7cJVnuoE7_yc8nlz_MZU8ZSPR6_OzdWUgxXrqJF1A\">" "$FILE"
+# Insert Google Search Console verification for zet-uzivo.com
+sed -i "/<meta name=\"description\"/a\\
+    <meta name=\"google-site-verification\" content=\"HBwILvARnqSZbwV7vUVtPD9KBYgUU33nTpaDzXsBFFM\">" "$FILE"
 
 # Switch title
 sed -i "s|<title>.*</title>|<title>${TITLE}</title>|i" "$FILE"
